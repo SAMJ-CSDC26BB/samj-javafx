4e5cf55cfd33c836b86cd855a019e2e6f3d71aab
Andreas Drozd <andreas.drozd@stud.fh-campuswien.ac.at>
2023-12-18 10:51:24 +0100

    created project

---
+++ samj/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.samj.samj {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.samj.samj to javafx.fxml;
+    exports com.samj.samj;
+}
