fded3a067a4ad9361bfe912feb2bb0811ba3afc0
Andreas Drozd <andreas.drozd@stud.fh-campuswien.ac.at>
2023-12-28 18:55:52 +0100

    start working on login window

---
+++ samj/src/main/java/com/samj/samj/LoginWindow.java
@@ -0,0 +1,59 @@
+package com.samj.samj;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.scene.text.Text; // Import the Text class
+
+import static javafx.application.Application.launch;
+
+public class LoginWindow {
+
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Login");
+
+        // Create the layout
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setVgap(10);
+        grid.setHgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        // Create the components
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 0);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 0);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 1);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 1);
+
+        Button btn = new Button("Sign in");
+        grid.add(btn, 1, 2);
+
+        final Text actionTarget = new Text();
+        grid.add(actionTarget, 1, 6);
+
+        // Add event handling (simple example)
+        btn.setOnAction(e -> {
+            actionTarget.setText("Sign in button pressed");
+        });
+
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
