7d35810b000599b0bc18c1c7e0299bd93cb7aa9c
joni97k <jknapp@alpenweb.net>
2023-12-30 21:47:24 +0100

    Request Simulator ausgebessert

--- samj/src/main/java/com/samj/backend/RequestSimulator.java
+++ samj/src/main/java/com/samj/backend/RequestSimulator.java
@@ -7,9 +7,9 @@ public class RequestSimulator {
     private static final String SERVER_ADDRESS = "localhost"; // or the actual server IP
     private static final int SERVER_PORT = 8080;
 
-    public static void main(String[] args) throws IOException {
+    public static void forwardingcheck(String argcallednumber) throws IOException {
         // Use the default phone number or the one provided as an argument
-        String calledNumber = args.length > 0 ? args[0] : DEFAULT_CALLED_NUMBER;
+        String calledNumber = argcallednumber == null ? DEFAULT_CALLED_NUMBER : argcallednumber;
 
         // Open a socket connection to the server
         try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
@@ -17,7 +17,7 @@ public class RequestSimulator {
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
             // Send an HTTP GET request to the server
-            String request = String.format("GET /forwardingcheck?callednumber=%s HTTP/1.1\r\nHost: %s\r\n\r\n",
+            String request = String.format("GET /forwardingcheck?argcallednumber=%s HTTP/1.1\r\nHost: %s\r\n\r\n",
                     calledNumber, SERVER_ADDRESS);
             out.write(request);
             out.flush();
