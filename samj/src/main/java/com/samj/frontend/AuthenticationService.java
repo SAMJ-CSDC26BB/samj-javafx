1d3abe7e085b3ef70b07b5710add8eb10d505ca1
Sergiu-Claudiu Iordanescu <sergiu-claudiu.iordanescu@stud.fh-campuswien.ac.at>
2024-01-18 18:31:39 +0100

    Add BCrypt class, implement hashing when creating a user and validation by login

--- samj/src/main/java/com/samj/frontend/AuthenticationService.java
+++ samj/src/main/java/com/samj/frontend/AuthenticationService.java
@@ -1,29 +1,23 @@
 package com.samj.frontend;
 
 
+import com.samj.shared.BCrypt;
 import com.samj.shared.DatabaseAPI;
 import com.samj.shared.UserDTO;
 public class AuthenticationService {
+    // TODO - remove at the end
+    // Users to use for login:
+    // username                 password
+    // ---------------------------------
+    // encryptedUser            test
+    // encryptedUserSecond      password
 
-
-    //public String hashPassword(String plainPassword) {
-    //    return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
-    //}
-
-    //public boolean checkPassword(String enteredPassword, String storedHash) {
-    //    return BCrypt.checkpw(enteredPassword, storedHash);
-    //}
-
-    //public boolean authenticate(String username, String password) {
-    //    // Retrieve the hashed password from the database for the given 'username'
-    //    String storedHash = retrieveHashFromDatabase(username);
-    //
-    //    // Check if the entered password matches the stored hash
-    //    return checkPassword(password, storedHash);
-    //}
     public static boolean authenticate(String username, String password) {
-        // TODO - when hash method is done, use it to compare the password
         UserDTO user = DatabaseAPI.loadUserByUsername(username);
-        return user != null && user.getPassword() != null && user.getPassword().equals(password);
+        return validateUserPassword(user, password);
+    }
+
+    private static boolean validateUserPassword(UserDTO user, String password) {
+        return user != null && user.getPassword() != null && BCrypt.checkpw(password, user.getPassword());
     }
 }
