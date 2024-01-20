87fd74c328d2e6462320c667dea4210991c3e386
Andreas Drozd <andreas.drozd@stud.fh-campuswien.ac.at>
2024-01-20 15:24:06 +0100

    create SettingsDTO.java

--- samj/src/main/java/com/samj/shared/SettingsDTO.java
+++ samj/src/main/java/com/samj/shared/SettingsDTO.java
@@ -1,4 +1,48 @@
 package com.samj.shared;
 
 public class SettingsDTO {
+
+    private String name;
+    private String serverURL;
+    private String serverPort;
+    private String db;
+
+    public SettingsDTO(String name, String serverURL, String serverPort, String db) {
+        this.name = name;
+        this.serverURL = serverURL;
+        this.serverPort = serverPort;
+        this.db = db;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getServerURL() {
+        return serverURL;
+    }
+
+    public void setServerURL(String serverURL) {
+        this.serverURL = serverURL;
+    }
+
+    public String getServerPort() {
+        return serverPort;
+    }
+
+    public void setServerPort(String serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    public String getDb() {
+        return db;
+    }
+
+    public void setDb(String db) {
+        this.db = db;
+    }
 }
